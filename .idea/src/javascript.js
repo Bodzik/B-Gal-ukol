0e25a7496f4d8cb39ea5cb1d3329139323305491
bodzik <LYmecek@seznam.cz>
2020-03-29 04:08:53 +0200

    uplně jinak

--- .idea/src/javascript.js
+++ .idea/src/javascript.js
@@ -1,14 +1,47 @@
-function changeImage(obj) {
-    var idx = obj.id;
-    var image = document.getElementById(idx);
-    if (image.src.match("https://raw.githubusercontent.com/Bodzik/B-Gal-ukol/master/yas.png")) {
-            image.src = "https://raw.githubusercontent.com/Bodzik/B-Gal-ukol/master/nope.png";
-        }
-        else {
-            image.src = "https://raw.githubusercontent.com/Bodzik/B-Gal-ukol/master/yas.png";
-        }
+function newUkol() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("input").value;
+  var t = document.createTextNode(inputValue);
+  var chckBox = document.createElement("input");
+
+  li.appendChild(t);
+  chckBox.type = "checkbox";
+  chckBox.className = "checkbox";
+  if (inputValue === '') {
+    alert("Nezapsal si název úkolu...");
+  } else {
+    li.style = "background-color:  #e6e6a3";
+    li.appendChild(chckBox);
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("input").value = "";
+
+  chckBox.onclick = function(){
+    li.style = "background-color: grey; text-decoration: line-through";
+  }
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
     }
-function changeFontSize(obj) {
-    var idx = obj.id;
-  document.getElementById(idx).style.fontSize = "xx-small";
+  }
 }
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+
+
